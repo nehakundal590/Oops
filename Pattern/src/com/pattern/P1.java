f507f0dd2905d1770e82d62ac04656c332e66031
neha <devineha590@gmail.com>
2026-01-15 00:00:08 +0530

    pattern

---
+++ Pattern/src/com/pattern/P1.java
@@ -0,0 +1,36 @@
+package com.pattern;
+
+public class P1 {
+	public static void main(String[] args) {
+		int n=19;
+	for(int i=0;i<=n-1;i++) {
+		for(int j=0;j<=n-1;j++) {
+			if(i==0&&j<=(n-1)/2||j==0 && i<=(n-1)/2||i+j<=(n-1)/2
+					||i==0 && j>=(n-1)/2||j==(n-1)&&i<=(n-1)/2||j-i>=(n-1)/2
+					||i>=(n-1)/2&&j==0||i==n-1&&j<=(n-1)/2||i-j>=(n-2)/2
+					||i==(n-1)&&j>=(n-1)/2||j==(n-1)&&i>=(n-1)/2|| i+j>=(n-1)+(n-1)/2) {
+				System.out.print("*");
+			}
+			else {
+				System.out.print(" ");
+			}
+		}
+		for(int j=0;j<=n-1;j++) {
+			if(i+j>=(n-1)/2&&i+j<=(n-1)+(n-1)/2
+					&&i-j<=(n-1)/2&&j-i<=(n-1)/2)
+					 {
+				System.out.print("*");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
+	}
+	
+}
+		
+	
+
+
