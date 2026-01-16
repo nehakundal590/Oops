3641e56dbe4e508b6e52ccb806cebca6b77a7a85
neha <devineha590@gmail.com>
2026-01-16 23:35:36 +0530

    pattern

---
+++ Pattern/src/com/pattern/T.java
@@ -0,0 +1,20 @@
+package com.pattern;
+
+public class T {
+
+public static void main(String[] args) {
+	int n=19;
+	for(int i=0;i<=n-1;i++) {
+		for(int j=0;j<=n-1;j++) {
+			if(i==0 || j==(n-1)/2) {
+				System.out.print("*");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println(" ");
+	}
+}
+
+}
+
